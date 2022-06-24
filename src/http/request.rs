85307c6109599d6b367cd14cdf4d102fea63b2a1
Saturn <s.nataniel10@gmail.com>
2022-06-24 07:57:12 +0200

    Read to buffer and add parse trait structure

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,8 +1,19 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     //Option - to represent the absence of value
     query_string: Option<String>,
     method: Method,
+}
+
+impl Request {}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        todo!()
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use std::io::Read;
 use std::net::TcpListener;
+use crate::http::Request;
+use std::convert::TryFrom;
 
 pub struct Server {
     addr: String,
@@ -17,17 +19,27 @@ impl Server {
         println!("Listening on {}", self.addr);
         // method. self takes the ownership of the struct, so the struct will b destroyed after the method execution
 
-        //unwrap returns the value or panics
-        //the address could already b in use, so this error is unrecoverable
+        // unwrap returns the value or panics
+        // the address could already b in use, so this error is unrecoverable
         let listener = TcpListener::bind(&self.addr).unwrap();
+        let mut buffer = [0; 1024]; // creates an array of size 1024 filled with zeros. type u8, so a bit more than 1KB
         loop {
-            match listener.accept(){
-                Ok((stream, addr)) => {
-                    // stream.read();
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            // &buffer[..] converts from &[u8; 1024] to &[u8]. Another way to convert: &buffer as &[u8]
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
                 }
-                Err(e) => println!("Filed to establish a connection: {}", e)
+                Err(e) => println!("Filed to establish a connection: {}", e),
             }
-
         }
     }
 }
